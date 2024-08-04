4cd3061c03bdc7c6f43d9fc42de35332c6f0851a
Hadixp <hadi.b.2002@gmail.com>
2024-08-04 14:14:26 +0430

    added: js file added to repository

---
+++ main.js
@@ -0,0 +1,9 @@
+let calc = 0 , number1 = 2 , number2 = 10;
+
+// below console.logs are just Basic math
+console.log('jam: ', number1+number2);
+console.log('menha:', number1-number2);
+console.log('zarb: ',number1*number2);
+console.log('taqsim: ',number1/number2);
+console.log('tavan: ',number1**number2);
+console.log('baqi mande: ',number1%number2);
